b7a7dd61734976a1799b341a5d522215295ede02
abassawo <abass.bayo@gmail.com>
2015-05-18 19:09:12 -0400

    Basic Calc Working Fine

--- ScientificCalculator_Build/app/src/main/java/abassawo/c4q/nyc/scientificcalculator_build/MainActivity.java
+++ ScientificCalculator_Build/app/src/main/java/abassawo/c4q/nyc/scientificcalculator_build/MainActivity.java
@@ -3,6 +3,9 @@ package abassawo.c4q.nyc.scientificcalculator_build;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,23 +13,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.text.DecimalFormat;
-
-
+import java.util.Iterator;
 
 
 import com.fathzer.soft.javaluator.DoubleEvaluator;
+import com.fathzer.soft.javaluator.Function;
+import com.fathzer.soft.javaluator.Parameters;
 
 public class MainActivity extends Activity{
-
     private String expression;
+    private String calcExpr;
+    private Button equalsButton;
+    private String delString;
     TextView calcDisplay;
     private double result;
-    private double answer;
+    private String ANS;
     private TextView answerDisplay;
     String resultStr;
     DoubleEvaluator expressionEvaluator;
+    private static final Function SQRT = new Function("sqrt", 1);
+    private static final Parameters params= DoubleEvaluator.getDefaultParameters(); // Gets the default DoubleEvaluator's parameters
 
-    //TODO : Implement functionality that clears text after err.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +42,71 @@ public class MainActivity extends Activity{
 
 
 
+        params.add(SQRT); // add the new sqrt function to Javaluator's parameters
+        expressionEvaluator = new DoubleEvaluator(params){
+            @Override
+            protected Double evaluate(Function function, Iterator arguments, Object evaluationContext) {
+                if (function == SQRT) {
+                    // Implements the new function
+                    return Math.sqrt((double) arguments.next());
+                } else {
+                    // If it's another function, pass it to DoubleEvaluator
+                    return super.evaluate(function, arguments, evaluationContext);
+                }
+            }
+        };
 
-        Button equalsButton = (Button) findViewById(R.id.equalsButton);
         final DecimalFormat doubleFormat = new DecimalFormat("0.00");
-        expression = "";
 
-        calcDisplay = (TextView) findViewById(R.id.tvExpression);
+        expression = "";
+        //calcExpr = expression;
+
         answerDisplay = (TextView) findViewById(R.id.tvAnswer);
-        expressionEvaluator = new DoubleEvaluator();
+        calcDisplay = (TextView) findViewById(R.id.tvExpression);
+        calcDisplay.setMovementMethod(new ScrollingMovementMethod());
+        equalsButton = (Button) findViewById(R.id.equalsButton);
         equalsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //ANS="";
                     try {
-                        result = expressionEvaluator.evaluate(expression);
-                        resultStr = doubleFormat.format(result); //avoid excessive decimals
+                        // calcExpr is the expression to be calculated. expression is just whats entered.
+                        if ((ANS!= null) && (expression.contains("ANS"))){
 
+                            expression.replace("ANS", resultStr.toString());  //assumes user prev calculated something
+                            Log.d("ANS", resultStr.toString());
+                        }
+                        calcExpr = expression; //can also create certain distinction.
+                        result = expressionEvaluator.evaluate(calcExpr); //calculate stuff
+                        Log.d("expr to be calculated", calcExpr);
+
+                        resultStr = doubleFormat.format(result); //avoid excessive decimals
                         resultStr = String.valueOf(result);
+
                         Log.d("test string", "resultStr");
                         if (resultStr.endsWith(".0")) {
                             resultStr = resultStr.substring(0, resultStr.indexOf('.')); //Integer cases!
-                            answerDisplay.setText(" = " + resultStr);
+                            answerDisplay.setText("=" + resultStr); //exper
+
                         } else {
                             answerDisplay.setText("" + resultStr); //perhaps format this for maximum # of digits after decimal place. "0.000"
                             // clear content for new calculations, unless an operation sign is selected. see comment above.
                         }
 
+
+
                     } catch (IllegalArgumentException e) {
                         answerDisplay.setText("Err");
                     }
-                expression = String.valueOf(resultStr); ///
+                    expression = String.valueOf(resultStr);
+                    calcExpr = null;
+                    expression = "";
+                    //returned to null. this way, you can still use a checker.
+                    // if calcexpr is null, when you do onNumber button clear calcdisplay before adding button
+                ANS = resultStr;
+                }
 
 
-            }
         });
     }
 
@@ -92,108 +132,169 @@ public class MainActivity extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
+    public void onOperationClick(View v){
+        switch (v.getId()) {
+            case R.id.delButton:
+                delString = calcDisplay.getText().toString();
+                int length = delString.length();
+                if (length > 0) {
+                    delString = delString.substring(0, length - 1);
+                    expression = delString;
+                } else {
+                    expression = "";
+                }
+                calcDisplay.setText(expression);
+                break;
+            case R.id.cancelButton: //rename this to clearButton.
+                calcExpr="";
+                expression="";
+                calcDisplay.setText("");
+                answerDisplay.setText("");
+                break;
+            //Operations
+            case R.id.minusButton:
+                if (expression == "" && ANS != null){
+                    expression += (ANS + "-");
+                    calcDisplay.setText(expression);
+                } else {
+                    expression += ("" + "-");
+                    calcDisplay.setText(expression);
+                }
+                break;
+            case R.id.plusButton:
+                if (expression == "" && ANS != null){
+                    expression += (ANS + "+");
+                    calcDisplay.setText(expression);
+                } else {
+                    expression += ("" + "+");
+                    calcDisplay.setText(expression);
+                }
+                    break;
+
+            case R.id.xButton:
+                if (expression == "" && ANS != null){
+                    expression += (ANS + "+");
+                    calcDisplay.setText(expression);
+                } else {
+                    expression += ("" + '*');
+                    calcDisplay.setText(expression);
+                }
+                break;
+            case R.id.divButton:
+                if (expression == "" && ANS != null){
+                    expression += (ANS + "+");
+                    calcDisplay.setText(expression);
+                } else {
+                    expression += ("" + '/');
+                    calcDisplay.setText(expression);
+                }
+                break;
+            case R.id.ansButton:
+                if (ANS == null){
+                    expression += "";
+                    calcDisplay.setText("");
+                } else if (expression.equals(ANS)){//works
+                    calcDisplay.setText(expression);
+                } else  {
+                    expression += ANS;
+                    calcDisplay.setText(expression);
+                }
+
+              }
+        }
+      public void onNumbersClicked(View v) {
+
+
+//
+        switch (v.getId()) {
+            case R.id.zeroButton:
+                expression += ("" + 0);
+                break;
+            case R.id.oneButton:
+                expression += ("" + 1);
+                break;
+            case R.id.twoButton:
+                expression += ("" + 2);
+                break;
+            case R.id.threeButton:
+                expression += ("" + 3);
+                break;
+            case R.id.fourButton:
+                expression += ("" + 4);
+                break;
+            case R.id.fiveButton:
+                expression += ("" + 5);
+                break;
+            case R.id.sixButton:
+                expression += ("" + 6);
+                break;
+            case R.id.sevenButton:
+                expression += ("" + 7);
+                break;
+            case R.id.eightButton:
+                expression += ("" + 8);
+                break;
+            case R.id.nineButton:
+                expression += ("" + 9);
+                break;
+        }
+        calcDisplay.setText(expression);
+    }
     public void onButtonClick(View v) {
             switch (v.getId()) {
-                case R.id.delButton:
-                    if (expression.length() >= 1) //or use scanner
-                    expression = expression.replace(expression.charAt(expression.length() - 1), ' ');
-                   // calcDisplay.setText(expression);
-                    break;
                 case R.id.decimalButton:
                     expression += ("" + '.');
                     //calcDisplay.setText(expression);
                     break;
                 case R.id.openParensButton:
                     expression += ("" + '(');
-                   // calcDisplay.setText(expression);
+                    // calcDisplay.setText(expression);
                     break;
                 case R.id.closeParensButton:
                     expression += ("" + ')');
-                   // calcDisplay.setText(expression);
+                    // calcDisplay.setText(expression);
                     break;
-                case R.id.cancelButton:
-                    expression = "";
-                    //calcDisplay.setText(expression);
-                    answerDisplay.setText("");
-                    break;
-                case R.id.ansButton:
-                    expression += resultStr;
-                    Log.d(" ans button working?", expression);
-                case R.id.zeroButton:
-                    expression += ("" + 0);
-                  //  calcDisplay.setText(expression);
-                    break;
-                case R.id.oneButton:
-                    expression += ("" + 1);
-                  //  calcDisplay.setText(expression);
-                    break;
-                case R.id.twoButton:
-                    expression += ("" + 2);
-                   // calcDisplay.setText(expression);
-                    break;
-                case R.id.threeButton:
-                    expression += ("" + 3);
-                    //calcDisplay.append("" + 3);
-                    break;
-                case R.id.fourButton:
-                    expression += ("" + 4);
-                   // calcDisplay.setText(expression);
-                    break;
-                case R.id.fiveButton:
-                    expression += ("" + 5);
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.sixButton:
-                    expression += ("" + 6);
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.sevenButton:
-                    expression += ("" + 7);
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.eightButton:
-                    expression += ("" + 8);
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.nineButton:
-                    expression += ("" + 9);
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.minusButton:
-                    expression += ("" + " -");
-                    //calcDisplay.setText(expression);
-                    break;
-                case R.id.plusButton:
-                    expression += ("" + "+ ");
-                   // calcDisplay.setText(expression);
-                    break;
-                case R.id.xButton:
-                    expression += ("" + '*');
-                   // calcDisplay.setText(expression);
-                    break;
-                case R.id.divButton:
-                    expression += ("" + '/');
-                    break;
+
                 case R.id.piButton:
-                    expression += ("" + 3.14159);
-                    //calcDisplay.setText(expression);
+                    expression += ("pi");
                     break;
                 case R.id.sinButton:
-//                    Button sinButton = ()
-                    expression += ("sin");
+                    expression += ("sin(");
                     //calcDisplay.setText(expression);
                     break;
                 case R.id.cosButton:
-                    expression += ("cos");
-                    //calcDisplay.setText(expression);
+                    expression += ("cos(");
                     break;
                 case R.id.tanButton:
-                    expression+=("tan");
-                    //calcDisplay.setText(expression);
+                    expression += ("tan(");
+                    break;
+                case R.id.lnButton:
+                    expression += ("ln(");
+                    break;
+                case R.id.exp_EXPButton:
+                    expression += ("^");
+                    break;
+                case R.id.eButton:
+                    expression += ("e");
+                    break;
+                case R.id.logButton:
+                    expression += ("log");
+                    break;
+                case R.id.sqrtButton:
+                    expression += ("sqrt");
+                    break;
+                case R.id.factorialButton:
+                    expression += ("!");
                     break;
 
-            }
-
-        calcDisplay.setText(expression);
     }
+            calcDisplay.setText(expression);
+
+    }
+
+
+
+
+
 }
+
